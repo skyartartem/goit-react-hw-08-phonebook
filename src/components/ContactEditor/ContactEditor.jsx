987be62a72f56430ab87abf276b5ac7ca88ddeac
Artem Riabchych <artemskyart@gmail.com>
2023-04-17 17:24:25 +0300

    fix-repetitive-contact

--- src/components/ContactEditor/ContactEditor.jsx
+++ src/components/ContactEditor/ContactEditor.jsx
@@ -1,28 +1,45 @@
 import { useDispatch } from 'react-redux';
 import { addContact } from 'redux/contacts/operations';
 import css from './ContactEditor.module.css';
+import { useSelector } from 'react-redux';
+import { selectAllContacts } from 'redux/contacts/selectors';
+import { toast } from 'react-hot-toast';
 
 export const ContactEditor = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(selectAllContacts);
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
     const name = form.elements.name.value;
     const number = form.elements.number.value;
-    if (name !== '' && number !== '') {
-      dispatch(addContact({name, number}));
-      form.reset();
+    if (name === '' && number === '') {
+      toast.error('Contact cannot be empty. Enter some text!');
+      //  alert('Contact cannot be empty. Enter some text!');
       return;
     }
-    alert('Contact cannot be empty. Enter some text!');
+    if (
+      contacts.some(
+        value => value.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      )
+    ) {
+      toast.error(`${name} is alredy in contacts`);
+      return;
+    }
+    dispatch(addContact({ name, number }));
+    form.reset();
   };
 
   return (
     <form className={css.form} onSubmit={handleSubmit}>
-      <label>Name: <input name="name" className={css.input} /></label>
-      <label>Number: <input name="number" type="tel" className={css.input} /></label>
-      
+      <label>
+        Name: <input name="name" className={css.input} />
+      </label>
+      <label>
+        Number: <input name="number" type="tel" className={css.input} />
+      </label>
+
       <button type="submit" className={css.button}>
         Add contact
       </button>
